c53dc454392c675b296475845c903bdeea811fcb
Bruce Shiundu <bruce.shiundu@sap.com>
2021-10-13 20:05:12 +0200

    Demo

--- test_calculator.py
+++ test_calculator.py
@@ -12,11 +12,20 @@ from app.views import calculator
 class TestCalculator:
 
     def test_addition(self):
+        print("Testing addition")
         assert 4 == calculator.add(2, 2)
+        print("Addition successful")
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
-        
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("SUCCESSFULLY DONE")
